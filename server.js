41be5b77ac27ec71833b7bec16b007392cdb7e49
Paul Bausch <pbausch@gmail.com>
2016-10-10 13:11:40 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+fs = require('fs');
+http = require('http');
+url = require('url');
+sharp = require('sharp');
+
+function return404(r) {
+	console.log('not found');
+    r.writeHead(404, {"Content-Type": "text/plain"});
+    r.end("404 Not found");
+	return;
+}
+
+function hasAllowedExtension(file, exts) {
+    return (new RegExp('(' + exts.join('|').replace(/\./g, '\\.') + ')$')).test(file);
+}
+
+http.createServer(function(req, res){
+	var request = url.parse(req.url, true);
+	var action = request.pathname;
+	
+	// Does this request contain an image action?
+	var regex = /\/(\d{1,3})x(\d{1,3})\//g;
+	result = regex.exec(action);
+	if (!result) {
+		return404(res);
+		return;
+	}
+	var actionDir = result[0];
+	var widthValue = parseInt(result[1]);
+	var heightValue = parseInt(result[2]);
+	
+	// Reset the request directory
+	action = action.replace(actionDir,"/");
+	reqFile = process.env.ROOT_DIR + action;
+		
+	// Is this an image request?
+	if (!hasAllowedExtension(reqFile, ['.jpg', '.JPG'])) {
+		return404(res);
+		return;
+	}
+	
+	// Does the image file exist?
+	try {
+	    fs.accessSync(reqFile, fs.F_OK);
+	} 
+	catch (e) {
+		return404(res);
+		return;
+	}
+	
+	// Resize the image according to the action
+	sharp(process.env.ROOT_DIR + action)
+	  .resize(widthValue,heightValue)
+	  .sharpen()
+	  .toBuffer()
+	  .then(function(outputBuffer) {
+			res.writeHead(200, {'Content-Type': 'image/jpg' });
+	 		res.end(outputBuffer, 'binary');	    
+	  });
+}).listen(8080);
+console.log('server started on 8080');
